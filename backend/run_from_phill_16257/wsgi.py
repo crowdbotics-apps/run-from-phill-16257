feee6d0a09acf0e496126fe3340e7de710f475d7
Crowdbotics <team@crowdbotics.com>
2020-04-25 20:40:01 +0000

    Update models

--- backend/run_from_phill_16257/wsgi.py
+++ backend/run_from_phill_16257/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'run_from_phill_16257.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "run_from_phill_16257.settings")
 
 application = get_wsgi_application()
